a7a1668b66944c2f46999108751f835090090dc7
khanhhai <khannhhai02012003@gmail.com>
2025-03-06 15:36:55 +0700

    ...

---
+++ src/ss3_java_overview/BasicMath.java
@@ -0,0 +1,24 @@
+package ss3_java_overview;
+
+import java.util.Scanner;
+
+public class BasicMath {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập số a");
+        int a = scanner.nextInt();
+
+        System.out.println("Nhập số b");
+        int b = scanner.nextInt();
+        int sum = a + b;
+        int difference = a - b;
+        int product = a * b;
+        double quotient = (double) a / b; // dùng double để có thể chia ra số thập phân
+
+        System.out.println("Tổng hai số a và b là: " + sum);
+        System.out.println("Hiệu hai số a và b là: " + difference);
+        System.out.println("Tích hai số a và b là: " + product);
+        System.out.println("Thương hai số a và b là: " + quotient);
+    }
+}
+
---
+++ src/ss3_java_overview/Exercise3.java
@@ -0,0 +1,25 @@
+package ss3_java_overview;
+
+import java.util.Scanner;
+
+public class Exercise3 {
+    public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.print("Nhập tên sản phẩm: ");
+            String tensanpham = scanner.nextLine();
+
+            System.out.print("Nhập số lượng sản phẩm: ");
+            int soluong = scanner.nextInt();
+
+            System.out.print("Nhập đơn giá sản phẩm: ");
+            double dongia = scanner.nextDouble();
+
+            double tien = soluong * dongia;
+            double thue = (tien * 10) / 100;
+
+            System.out.println("Tên sản phảm là: " + tensanpham);
+            System.out.println("Tổng tiền là: " + tien);
+            System.out.println("Thuế phải trả là: " + thue);
+        }
+    }
